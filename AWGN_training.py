9c368ffed97ca5febd1548e9f39f9844eb70103d
attilasimko <attila.simko@umu.se>
2020-10-20 11:35:07 +0200

    New stuff

--- AWGN_training.py
+++ AWGN_training.py
@@ -25,7 +25,7 @@ GPU = int(args.gpu)
 
 def output_range(x):
     from tensorflow.keras import backend as K
-    rng = 0.2
+    rng = 0.9
     x = tensorflow.where(K.greater(x, -rng), x, -rng*K.ones_like(x))
     x = tensorflow.where(K.less(x, rng), x, rng*K.ones_like(x))
     return x
@@ -47,8 +47,8 @@ def implicit_relation(tensor):
     a = tensor[0]
     b = tensor[1]
     c = tensorflow.math.subtract(a, b)
-    #scale = K.mean(c, axis=(1, 2, 3))
-    #c = tensorflow.math.subtract(c, scale[:, None, None, None])
+    # scale = K.mean(c, axis=(1, 2, 3))
+    # c = tensorflow.math.subtract(c, scale[:, None, None, None])
     return c
     
 def implicit_correction(tensor):
@@ -68,10 +68,10 @@ def build_model():
     from tensorflow.keras.layers import Input, MaxPooling2D, Conv2D, Conv2DTranspose, BatchNormalization, Dropout, UpSampling2D, Concatenate, Dropout, add
     dropout_rate = 0.2
     l2reg = 0.0001
-    filt = 4
-    kernel_size = 10
+    filt = 2
+    kernel_size = 15
     
-    inp = Input(shape=(64, 64, 1))
+    inp = Input(shape=(256, 256, 1))
     x = Conv2D(filt, kernel_size, padding = 'same')(inp)
     x = Conv2D(filt*2, kernel_size, padding = 'same')(x)
     x = Dropout(dropout_rate)(x)
@@ -82,13 +82,13 @@ def build_model():
     x = Conv2D(filt*16, kernel_size, padding = 'same')(x)
     x = Conv2D(1, kernel_size, padding = 'same')(x)
 
-    x = Lambda(output_range)(x)
+    # x = Lambda(output_range)(x)
 
     return Model(inputs=[inp], outputs=[x])
 
 def build_implicit():
-    im1 = Input(shape=(64, 64, 1))
-    im2 = Input(shape=(64, 64, 1))
+    im1 = Input(shape=(256, 256, 1))
+    im2 = Input(shape=(256, 256, 1))
     im1out1 = model(im1)
     im2out1 = model(im2)
 
@@ -102,59 +102,48 @@ def build_implicit():
     im2n = Lambda(implicit_correction)([im2, im2out1])
     reg2 = model(im2n)
 
-    return Model(inputs=[im1, im2], outputs=[out1])#, reg1, reg2])
+    return Model(inputs=[im1, im2], outputs=[out1, reg1, reg2])
 
 if GPU >= 0:
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
     os.environ["CUDA_VISIBLE_DEVICES"] = str(GPU)
 
 noise_size = 0.01
-orig=cv2.resize(mpimg.imread('orig.png')[:, :, 0], dsize=(64, 64), interpolation=cv2.INTER_LANCZOS4)
+orig=cv2.resize(mpimg.imread('orig.png')[:, :, 0], dsize=(256, 256), interpolation=cv2.INTER_LANCZOS4)
 orig = np.array(np.interp(orig, (orig.min(), orig.max()), (0, 1)), dtype=np.float32)
 orig_noise = np.random.normal(0, noise_size, size=np.shape(orig))
 noisy = orig + orig_noise
 noisy = np.array(np.interp(noisy, (noisy.min(), noisy.max()), (0, 1)), dtype=np.float32)
 # np.savez('/home/attila/out' + str(GPU) + '/_data', orig = orig, noisy = noisy)
-print(str(psnr(noisy, orig)))
 
-n_epochs = 100
-n_its = 1000
+n_epochs = 10000
+n_its = 500
 batch_size = 1
-lr = 0.000001
+lr = 0.001
 noisy_dataset = np.repeat(np.expand_dims(np.expand_dims(noisy, axis=2), axis=0), batch_size, axis=0)
 zeros = np.zeros_like(noisy_dataset)
 
 
 # Correct images.
 model = build_model()
-model.compile(loss=['mean_squared_error'], optimizer=tensorflow.keras.optimizers.Adam(lr))
+model.compile(loss=['mean_squared_error'], optimizer=tensorflow.keras.optimizers.Nadam(lr))
 
 implicit = build_implicit()
 implicit.compile(optimizer=tensorflow.keras.optimizers.Nadam(lr), 
-              loss=['mean_squared_error'],
-              loss_weights=[1])
-# implicit.compile(optimizer=tensorflow.keras.optimizers.Nadam(lr), 
-#               loss=['mean_squared_error', 'mean_squared_error', 'mean_squared_error'],
-#               loss_weights=[1, 0.5, 0.5])
+              loss=['mean_squared_error', 'mean_squared_error', 'mean_squared_error'],
+              loss_weights=[1, 0.5, 0.5])
 
-              
-# IN_ID = '/home/attila/data/DS0044s/IN'
-# IN = DataGenerator(IN_ID,
-#                     inputs=[['image', False, 'float32']],
-#                     outputs=[],
-#                     batch_size=batch_size,
-#                     shuffle=True)
-#
-# print(implicit.count_params())
 file_id = 0
 
 for epoch in range(n_epochs):
     loss = []
+    print(np.mean(orig_noise**2))
+    print(str(psnr(noisy, orig)))
     for idx in range(int(n_its / batch_size)):
         noise = np.random.normal(0, noise_size, size=np.shape(noisy_dataset))
         full_data = noisy_dataset + noise
         full_data = np.array(np.interp(full_data, (full_data.min(), full_data.max()), (0, 1)), dtype=np.float32)
-        hist = implicit.fit([full_data, noisy_dataset], [noise], verbose=0)
+        hist = implicit.fit([full_data, noisy_dataset], [noise, zeros, zeros], verbose=0)
         loss.append(hist.history['loss'][0])
 
     print('Epoch ' + str(epoch) + ':    ' + str(np.round(np.mean(loss), 10)))
@@ -165,6 +154,8 @@ for epoch in range(n_epochs):
     bins = np.linspace(np.min([hist_orig, hist_pred]), np.max([hist_orig, hist_pred]), 50)
     
     img = noisy - img[0, :, :, 0]
+    img = np.interp(img, (img.min(), img.max()), (0, 1))
+
     print(str(psnr(img, orig)))
 
 
--- BFC_testing.py
+++ BFC_testing.py
@@ -1,6 +1,5 @@
 import sys
 import scipy
-import tensorflow
 from tensorflow.keras import optimizers, metrics
 import numpy as np
 import os
@@ -15,6 +14,8 @@ import matplotlib.pyplot as plt
 from tensorflow.keras.models import load_model
 import sys
 random.seed(2019)
+# sys.path.append("C:/Users/attil/Documents/DRS/drs/")
+# from MLTK.data import BiasGenerator
 
 
 """
@@ -31,92 +32,83 @@ Stage 3:
 """
 
 # model = load_model('Experiments/CompNets/000/12.h5')
-model = load_model('C:/Users/attil/Downloads/out/100.h5')
+model = tensorflow.keras.models.load_model('C:/Users/attil/Downloads/out/10.h5', compile=False)
 dataset = '1TA40.npz'
 
 with np.load(dataset) as npzfile:
-    im = npzfile['image']
-    data = npzfile['data']
-    tissue = npzfile['tissue']
+    im = npzfile['image'][50, :, :]
+    data = npzfile['data'][50, :, :]
+    tissue = npzfile['tissue'][50, :, :]
 
 BFG = BiasGenerator(size=(256, 256), batch_size=1)
 
-inputImage, inputBias = val_gen.__getitem__(0)
-for fileID in range(20):
-    img2 = inputImage[0][0, int(fileID*5), :, :, 0]
-    true_bias = inputBias[0][0, int(fileID*5), :, :, 0]
+bias = BFG.GetFields()
+bias = bias[:, :, 0]
+np.savez('data', image = im, data = data, bias = bias)
+img2 = im
 
-    bias = BFG.GetFields()
-    bias = bias[:, :, 0]
-    img1 = img2 * bias
-    img1 = img1 / np.max(img1)
-    #bias = inputBias[0][fileID, :, :, 0]
-    #bias = cv2.resize(bias, dsize=(256, 256),
-    #                  interpolation=cv2.INTER_LANCZOS4)
-    #bias = bias / np.mean(bias)
+img1 = im * bias
+img1 = img1 / np.max(img1)
 
-    plt.figure(1, figsize=(90, 10))
-    plt.subplot(191)
-    plt.imshow(img1*bias, cmap='gray')
-    plt.axis('off')
-    plt.subplot(192)
-    plt.imshow(img2, cmap='gray')
-    plt.axis('off')
-    plt.subplot(193)
-    plt.imshow(bias, cmap='gray')
-    plt.axis('off')
+plt.figure(1, figsize=(20, 5))
+plt.subplot(2, 5, 1)
+plt.imshow(img2, cmap='gray')
+plt.axis('off')
+plt.subplot(2, 5, 6)
+plt.imshow(bias, cmap='gray')
+plt.axis('off')
 
-    img1 = np.expand_dims(np.expand_dims(img1, axis=0), axis=4)
-    img2 = np.expand_dims(np.expand_dims(img2, axis=0), axis=4)
-
-    bias1 = model.predict(img1)
-    bias2 = model.predict(img2)
-    bias1 = bias1[0, :, :, 0]
-    bias2 = bias2[0, :, :, 0]
-
-    bias1 = bias1 / np.mean(bias1)
-    bias2 = bias2 / np.mean(bias2)
-    bias_pred = np.divide(bias1, bias2)
-    bias_pred = bias_pred / np.mean(bias_pred)
-    vmin1, vmax1 = np.min(bias1), np.max(bias1)
-    vmin2, vmax2 = np.min(bias2), np.max(bias2)
-    vmin = np.min([vmin1, vmin2])
-    vmax = np.max([vmax1, vmax2])
-
-    plt.subplot(194)
-    plt.imshow(bias1, cmap='gray')
-    plt.axis('off')
-    plt.subplot(195)
-    plt.imshow(bias2, cmap='gray')
-    plt.axis('off')
-    plt.subplot(196)
-    plt.imshow(bias_pred, cmap='gray')
-    plt.axis('off')
+plt.subplot(2, 5, 2)
+plt.imshow(img1, cmap='gray')
+plt.axis('off')
 
 
-    img1 = np.divide(img1[0, :, :, 0], bias1)
-    img2 = np.divide(img2[0, :, :, 0], bias2)
-    img1 = img1 / np.max(img1)
-    img2 = img2 / np.max(img2)
+img1 = np.expand_dims(np.expand_dims(img1, axis=0), axis=4)
+img2 = np.expand_dims(np.expand_dims(img2, axis=0), axis=4)
 
-    img1 = np.expand_dims(np.expand_dims(img1, axis=0), axis=4)
-    img2 = np.expand_dims(np.expand_dims(img2, axis=0), axis=4)
+bias1 = model.predict(img1)
+bias2 = model.predict(img2)
+bias1 = bias1[0, :, :, 0]
+bias2 = bias2[0, :, :, 0]
+bias_pred = np.divide(bias1, bias2)
+bias_pred = bias_pred / np.mean(bias_pred)
 
-    bias1 = model.predict(img1)
-    bias2 = model.predict(img2)
-    bias1 = bias1[0, :, :, 0]
-    bias2 = bias2[0, :, :, 0]
-    bias1 = bias1 / np.mean(bias1)
-    bias2 = bias2 / np.mean(bias2)
+img1 = np.divide(img1[0, :, :, 0], bias1)
+img2 = np.divide(img2[0, :, :, 0], bias2)
+img1 = img1 / np.max(img1)
+img2 = img2 / np.max(img2)
 
-    plt.figure(1)
-    plt.subplot(197)
-    plt.imshow(bias1, cmap='gray', vmin=vmin, vmax=vmax)
-    plt.axis('off')
-    plt.subplot(198)
-    plt.imshow(bias2, cmap='gray', vmin=vmin, vmax=vmax)
-    plt.axis('off')
-    plt.subplot(199)
-    plt.imshow(true_bias, cmap='gray', vmin=vmin, vmax=vmax)
-    plt.axis('off')
-    plt.show()
+img1 = np.expand_dims(np.expand_dims(img1, axis=0), axis=4)
+img2 = np.expand_dims(np.expand_dims(img2, axis=0), axis=4)
+
+reg1 = model.predict(img1)
+reg2 = model.predict(img2)
+reg1 = reg1[0, :, :, 0]
+reg2 = reg2[0, :, :, 0]
+
+vmin1, vmax1 = np.min(bias1), np.max(bias1)
+vmin2, vmax2 = np.min(bias2), np.max(bias2)
+vmin3, vmax3 = np.min(bias_pred), np.max(bias_pred)
+vmin4, vmax4 = np.min(reg1), np.max(reg1)
+vmin5, vmax5 = np.min(reg2), np.max(reg2)
+vmin = np.min([vmin1, vmin2, vmin3, vmin4, vmin5])
+vmax = np.max([vmax1, vmax2, vmax3, vmax4, vmax5])
+
+plt.subplot(2, 5, 3)
+plt.imshow(bias1, cmap='gray', vmin=vmin, vmax=vmax)
+plt.axis('off')
+plt.subplot(2, 5, 8)
+plt.imshow(bias2, cmap='gray', vmin=vmin, vmax=vmax)
+plt.axis('off')
+
+plt.subplot(2, 5, 4)
+plt.imshow(reg1, cmap='gray', vmin=vmin, vmax=vmax)
+plt.axis('off')
+plt.subplot(2, 5, 9)
+plt.imshow(reg2, cmap='gray', vmin=vmin, vmax=vmax)
+plt.axis('off')
+
+plt.subplot(1, 5, 5)
+plt.imshow(bias_pred, cmap='gray', vmin=vmin, vmax=vmax)
+plt.axis('off')
+plt.show()
